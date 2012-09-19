e194290950a16d08f71ec903a9380a11f1ecd895
Mick Staugaard <mick@staugaard.com>
2012-09-19 11:03:53 -0700

    testing the worker

---
+++ test/worker_test.rb
@@ -0,0 +1,38 @@
+require_relative 'test_helper'
+
+describe Adrian::Worker do
+  describe "#perform" do
+    before { @item = 2}
+
+    it "should report back to the boss" do
+      worker_class = Class.new(Adrian::Worker) do
+        def work; item + 2; end
+      end
+
+      worker = worker_class.new(@item)
+      boss = MiniTest::Mock.new
+      worker.report_to(boss)
+
+      boss.expect(:work_done, nil, [worker, @item, nil])
+      worker.perform
+
+      boss.verify
+    end
+
+    it "should NEVER raise an exception" do
+      worker_class = Class.new(Adrian::Worker) do
+        def work; raise "STRIKE!"; end
+      end
+
+      worker = worker_class.new(@item)
+      boss = MiniTest::Mock.new
+      worker.report_to(boss)
+
+      boss.expect(:work_done, nil, [worker, @item, RuntimeError])
+
+      worker.perform
+
+      boss.verify
+    end
+  end
+end
